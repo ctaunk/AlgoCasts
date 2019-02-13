2569e1fc0f247494929c44ed22ec71b3d1e42e53
Chandresh Taunk <ctaunk@gmail.com>
2019-02-12 19:59:04 -0800

    implementation for anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,43 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    const strA = canonicalize(stringA);
+    const strB = canonicalize(stringB);
+    if (strA.length !== strB.length) {
+        return false;
+    }
+    const chrMap = makeCharMap(strA);
+    for (let ch of strB) {
+        if (chrMap[ch]) {
+            chrMap[ch]--;
+            if (chrMap[ch] === 0) {
+                chrMap.delete(ch);
+            }
+        } else {
+            return false;
+        }
+    }
+    return chrMap.size === 0;
+}
+
+function canonicalize(str) {
+    const lowerStr = str.toLowerCase();
+    var converted = '';
+    for (let ch of str) {
+        if (ch >= 'a' && ch <= 'z') {
+            converted = converted + ch;
+        }
+    }
+    return converted;
+}
+
+function makeCharMap(str) {
+    const chrMap = new Map();
+    for (let ch of str) {
+        chrMap[ch] = chrMap[ch] + 1 || 1
+    }
+    return chrMap;
+}
 
 module.exports = anagrams;
